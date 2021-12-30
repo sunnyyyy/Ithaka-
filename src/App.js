c4e8e41f806505f65cdc5fab815bc8375000500b
sunnyyyy <sunnylaio123@gmail.com>
2021-12-30 23:16:17 +0000

    NEWWWWW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import Icon from '@mui/material/Icon';
 function App() {
+ const style1= {
+   padding:"50px"
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={style1}>
+      <Accordion>
+        <AccordionSummary          
         >
-          Learn React
-        </a>
-      </header>
+          <Typography>As you set out for Ithaka</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          hope your road is a long one,
+          </Typography>
+        </AccordionDetails>
+        <Accordion>
+        <AccordionSummary
+        >
+          <Typography>full of adventure, full of discovery.</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Laistrygonians, Cyclops, angry Poseidon—don’t be afraid of them:
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>you’ll never find things like that on your way</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p>as long as you keep your thoughts raised high,</p>
+          <p>as long as a rare excitement</p>
+          <p>stirs your spirit and your body.</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Laistrygonians, Cyclops,
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+         <p> wild Poseidon—you won’t encounter them</p>
+         <p>unless you bring them along inside your soul,</p>
+         <p>unless your soul sets them up in front of you.</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Hope your road is a long one.
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p> May there be many summer mornings when,</p>
+         <p>with what pleasure, what joy,</p>
+         <p>you enter harbors you’re seeing for the first time;</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>may you stop at Phoenician trading stations
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p> to buy fine things,</p>
+         <p>mother of pearl and coral, amber and ebony,</p>
+         <p>sensual perfume of every kind—</p>
+         <p>as many sensual perfumes as you can;</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>and may you visit many Egyptian cities
+
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          to learn and go on learning from their scholars.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Keep Ithaka always in your mind.
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+         <p> Arriving there is what you’re destined for.</p>
+          <p>But don’t hurry the journey at all.</p>
+
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Better if it lasts for years,
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p>so you’re old by the time you reach the island,</p>
+          <p>wealthy with all you’ve gained on the way,</p>
+          <p>not expecting Ithaka to make you rich.</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Ithaka gave you the marvelous journey.
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p>Without her you wouldn't have set out.</p>
+          <p>She has nothing left to give you now.</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>And if you find her poor, Ithaka won’t have fooled you.
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          <p>Wise as you will have become, so full of experience,</p>
+          <p>you’ll have understood by then what these Ithakas mean.</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+        >
+          <Typography>Ithaka BY C. P. CAVAFY
+         </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Happy Birthday:)
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      
+      </Accordion>
+     
+     
     </div>
   );
 }
